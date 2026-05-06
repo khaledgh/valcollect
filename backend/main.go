807e8fb308074dee05db59f12849e7905a9e3334
Khaled Ghourani <Khaledghourani4@gmail.com>
2026-05-06 17:05:59 +0300

    fixes

--- backend/main.go
+++ backend/main.go
@@ -42,7 +42,7 @@ func main() {
 
 	// CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "https://sahmeeinstitute.com", "https://www.sahmeeinstitute.com", "https://sahcollect.com", "https://www.sahcollect.com", "https://sahcollect.com", "https://www.sahcollect.com"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "https://demo.valcollect.com", "https://www.demo.valcollect.com", "https://valcollect.com", "https://www.valcollect.com"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length", "Content-Disposition"},
